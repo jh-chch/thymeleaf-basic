e4abe893eac25708657d44c59b4dc95fbef3de3a
jh-chch <jhchoig9@gmail.com>
2024-04-02 22:10:57 +0900

    Expression Basic Objects

--- src/main/java/study/thymeleafbasic/basic/BasicController.java
+++ src/main/java/study/thymeleafbasic/basic/BasicController.java
@@ -5,11 +5,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -33,7 +37,7 @@ public class BasicController {
         List<User> list = new ArrayList<>();
         list.add(userA);
         list.add(userB);
-        
+
         Map<String, User> map = new HashMap<>();
         map.put("userA", userA);
         map.put("userB", userB);
@@ -44,7 +48,24 @@ public class BasicController {
 
         return "basic/variable";
     }
-    
+
+    @GetMapping("/basic-objects")
+    public String basicObject(Model model, HttpServletRequest request,
+            HttpServletResponse response, HttpSession session) {
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        session.setAttribute("sessionData", "SESSION-DATA");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello" + data;
+        }
+    }
+
     @Getter
     @Setter
     @AllArgsConstructor
